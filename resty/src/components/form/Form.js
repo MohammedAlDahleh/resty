4ff2627587af880bc1b529c12b7dd1858326373b
MohammedAlDahleh <mohammedd993@gmail.com>
2022-08-11 14:42:48 +0300

    uplode class 27 usestate code

--- resty/src/components/form/Form.js
+++ resty/src/components/form/Form.js
@@ -5,9 +5,10 @@ function Form(props) {
     const handleSubmit = e => {
         e.preventDefault();
         const formData = {
-            method: 'GET',
-            url: 'https://pokeapi.co/api/v2/pokemon',
+            method:e.target[1].value,
+            url: e.target[0].value,
         };
+        console.log("----------------------", e.target[1].value);
         props.handleApiCall(formData);
     }
 
@@ -16,15 +17,17 @@ function Form(props) {
             <form onSubmit={handleSubmit}>
                 <label className='label-input'>
                     <span>URL: </span>
-                    <input name='url' type='text' className='input'/>
-                    <button type="submit"  className='btn'>GO!</button>
+                    <input name='url' type='text' className='input' />
                 </label>
-                <label className="methods">
-                    <span id="get">GET</span>
-                    <span id="post">POST</span>
-                    <span id="put">PUT</span>
-                    <span id="delete">DELETE</span>
-                </label>
+                <label for="method">Choose a method:</label>
+
+                <select className="methods">
+                    <option value="get">get</option>
+                    <option value="post">post</option>
+                    <option value="update">update</option>
+                    <option value="delete">delete</option>
+                </select>
+                    <button type="submit" className='btn'>GO!</button>
             </form>
         </>
     )

18cda5262dd79b4923df993541fba7097d4d8e98
MohammedAlDahleh <mohammedd993@gmail.com>
2022-08-11 22:51:55 +0300

    m

--- resty/src/components/form/Form.js
+++ resty/src/components/form/Form.js
@@ -5,31 +5,28 @@ function Form(props) {
     const handleSubmit = e => {
         e.preventDefault();
         const formData = {
-            method:e.target[1].value,
-            url: e.target[0].value,
+            method: e.target[1].value,
+            url: e.target[0].value
         };
-        console.log("----------------------", e.target[1].value);
         props.handleApiCall(formData);
     }
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label className='label-input'>
-                    <span>URL: </span>
-                    <input name='url' type='text' className='input' />
-                </label>
-                <label for="method">Choose a method:</label>
+    return ( <> <form onSubmit={handleSubmit}>
+        <label className='label-input'>
+            <span>URL:
+            </span>
+            <input name='url' type='text' className='input'/>
+        </label>
+        <label className="methods">Choose a method:</label>
 
-                <select className="methods">
-                    <option value="get">get</option>
-                    <option value="post">post</option>
-                    <option value="update">update</option>
-                    <option value="delete">delete</option>
-                </select>
-                    <button type="submit" className='btn'>GO!</button>
-            </form>
-        </>
+        <select className="methods">
+            <option value="get">Get</option>
+            <option value="post">Post</option>
+            <option value="update">Update</option>
+            <option value="delete">Delete</option>
+        </select>
+        <button type="submit" className='btn'>GO!</button>
+    </form> </>
     )
 }
 export default Form;
